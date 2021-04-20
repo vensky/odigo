cc73928d00d6f25fd69f23ce13c0d94a33a64700
vensky <ivan.fedotovskikh@gmail.com>
2021-04-20 14:26:21 +0500

    Добавление проекта

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,1 @@
 "use strict";
-
-@@include("../../node_modules/swiper/swiper-bundle.js");
-@@include("../../node_modules/imask/dist/imask.min.js");
-@@include("swiper_init.js");
-@@include("nav.js");
-@@include("modals.js");
-@@include("mask.js");
