29d13ff702c356af4ac6534c36c56e52c0759876
vensky <ivan.fedotovskikh@gmail.com>
2021-04-28 14:16:45 +0500

    Добавляет адаптив header

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,17 @@
 "use strict";
+
+const $body = document.querySelector('.js-body');
+const $header = document.querySelector('.js-header');
+const $btnNav = document.querySelector('.js-btnNav');
+const $nav = document.querySelector('.js-nav');
+
+window.addEventListener('scroll', () => {
+    window.pageYOffset > 0 ? $header.classList.add('header--scroll') : $header.classList.remove('header--scroll');
+});
+
+$btnNav.addEventListener('click', () => {
+    $btnNav.classList.toggle('btn-nav--active');
+    $nav.classList.toggle('nav--active');
+    $header.classList.toggle('header--active');
+    $body.classList.toggle('no-scroll');
+});
