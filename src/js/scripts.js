8db987b4570c75f6b2a1f17cd1a591b4dfd2762c
vensky <ivan.fedotovskikh@gmail.com>
2021-05-18 16:55:23 +0500

    Добавляет блок footer

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,6 +4,8 @@ const $body = document.querySelector('.js-body');
 const $header = document.querySelector('.js-header');
 const $btnNav = document.querySelector('.js-btnNav');
 const $nav = document.querySelector('.js-nav');
+const $accordeon = document.querySelector('.js-accordion');
+
 
 window.addEventListener('scroll', () => {
     window.pageYOffset > 0 ? $header.classList.add('header--scroll') : $header.classList.remove('header--scroll');
@@ -15,3 +17,8 @@ $btnNav.addEventListener('click', () => {
     $header.classList.toggle('header--active');
     $body.classList.toggle('no-scroll');
 });
+
+$accordeon.addEventListener('click', (e) => {
+    e.target.nextElementSibling.classList.toggle('show');
+    console.log(e.target.nextElementSibling)
+})
